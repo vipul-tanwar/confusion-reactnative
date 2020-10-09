198d55959ef855a3aa679342b218cdaf7b336fa2
Vipul Tanwar <vipultanwar45@gmail.com>
2020-10-09 12:59:17 +0530

    Social Sharing

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
@@ -75,6 +75,16 @@ function RenderDish(props) {
             return true;
         }
     }) 
+
+    const shareDish = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: title + ':  ' + message + ' ' + url,
+            url: url
+        },{
+            dialogTitle: 'Share' + title
+        });
+    }
     
         if (dish != null) {
             return(
@@ -97,14 +107,24 @@ function RenderDish(props) {
                         onPress={() => props.favorite ? console.log('Already favourite') : props.onPress()}
                     />
   
-                        <Icon
+                    <Icon
                         raised 
                         reverse
                         name= 'pencil'
                         type='font-awesome'
                         color='#512DA8'
                         onPress={() => props.onPressComment()}
-                        />
+                    />
+
+                    <Icon 
+                        raised
+                        reverse
+                        name='share'
+                        type='font-awesome'
+                        color='#51D2A8'
+                        style={styles.cardItem}
+                        onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)}
+                    />
                         
                 </View>
                 </Card>
